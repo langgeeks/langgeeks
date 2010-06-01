9ec2dde6953ef738011333a26c233398a3a7aa2e
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2010-06-01 23:03:29 +0300

    first check-in

---
+++ ReversiSuite.scala
@@ -0,0 +1,11 @@
+import org.scalatest.junit.AssertionsForJUnit
+import org.junit.Test
+import org.junit.Assert._
+
+class ReversiSuite extends AssertionsForJUnit {
+ 
+ @Test
+  def verifySomeStuff() {
+    assertEquals("1", 1.toString())
+  }
+}
