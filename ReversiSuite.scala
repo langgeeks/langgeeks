8ff89c138bb3d9d564695e2662451866c31bac24
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2010-06-03 16:33:59 +0300

    First test. Test board setup.

--- ReversiSuite.scala
+++ ReversiSuite.scala
@@ -9,4 +9,15 @@ class ReversiSuite extends AssertionsForJUnit {
     assertEquals("1", 1.toString())
   }
 
+  @Test
+  def startBoardIsSetupCorrectly() {
+     val board = new Board()
+     assertEquals(Square.Black, board.squares(3)(3))
+     assertEquals(Square.White, board.squares(3)(4))
+     assertEquals(Square.White, board.squares(4)(3))
+     assertEquals(Square.Black, board.squares(4)(4))
+  }
+
+
 }
+
