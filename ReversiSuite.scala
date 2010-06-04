95ef3de58a06d94c8937276c61eb1deced8e4222
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2010-06-04 14:32:57 +0300

    Removed type stuff. Also added first possible move test.

--- ReversiSuite.scala
+++ ReversiSuite.scala
@@ -23,7 +23,7 @@ class ReversiSuite extends AssertionsForJUnit {
 
   @Test
   def oneLineOfDotsIsEightEmptySquares() {
-      val row:Array[Square.Value] = board.createRow("........")
+      val row = board.createRow("........")
       val emptyRow = Array(Square.Empty, Square.Empty, Square.Empty, Square.Empty, Square.Empty, Square.Empty, Square.Empty, Square.Empty)
       assertEquals(emptyRow.size, row.size)
       for (square <- row)
@@ -32,12 +32,19 @@ class ReversiSuite extends AssertionsForJUnit {
 
   @Test
   def oneLineWithAllTypesOfSquares() {
-      val row:Array[Square.Value] = board.createRow(".WBO")
+      val row = board.createRow(".WBO")
       assertEquals(Square.Empty, row(0))
       assertEquals(Square.White, row(1))
       assertEquals(Square.Black, row(2))
       assertEquals(Square.Possible, row(3))
   }
 
+  @Test
+  def blackHasOnePossibleMoveToTheLeftOnOneLine() {
+      val row = board.createRow("...WB...")
+      val expected = board.createRow("..OWB...")
+      val evaluated = board.evaluate(row)
+      assert(expected.toList == evaluated.toList)
+  }
 }
 
