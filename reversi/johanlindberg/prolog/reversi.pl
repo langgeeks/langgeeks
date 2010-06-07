a2b7598e02e5ea7088ae7444000f60e3d1f4b87f
Johan Lindberg <johan@pulp.se>
2010-06-07 21:51:19 +0200

    First attempt.
    This fails! Thank God for trace. Turns out I can't use C+1 and C+2. Instead
    of trying to unify with a position fact where the last term is 1 it tries to
    unify with 0+1. I've got to figure out a way to make it handle the numeric
    variables as numbers.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -1,8 +1,11 @@
 % reversi kata
 
-% board data
-
 position('.',0,0).
 position('B',0,1).
 position('W',0,2).
 
+legal_move(R,C,Player) :-
+    position('.',R,C),
+    position(Opponent,R,C+1),
+    Opponent \= Player,
+    position(Player,R,C+2).
