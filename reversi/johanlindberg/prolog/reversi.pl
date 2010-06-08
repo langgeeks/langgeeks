74c223b1dd40cdac3e2a97432e7823f401c152a8
Johan Lindberg <johan@pulp.se>
2010-06-08 11:51:05 +0200

    Added rules for searching left, up and down.
    This is a copy-paste job. I have no idea if this can be expressed in a more
    compact manner or not. It probably can, I just can't think of it. Now there
    are 4 legal_move rules, one for each direction, and a total of 8 chain rules.
    One recursive and one terminating for each direction.
    
    Unfortunately it only finds 3 out of the 4 solutions I expected it to.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -24,10 +24,39 @@ position('.',3,3).
 
 % ---------------
 
+% a position is a legal move if it
+% is empty and has a chain to the
+% right, left, up- or downwards of
+% it.
+
 legal_move(R,C,Player) :-
-    position('.',R,C), % position must be empty
+    position('.',R,C),
+
     C1 is C + 1,
     rchain(R,C1,Player).
+
+legal_move(R,C,Player) :-
+    position('.',R,C),
+
+    C1 is C - 1,
+    lchain(R,C1,Player).
+
+legal_move(R,C,Player) :-
+    position('.',R,C),
+
+    R1 is R + 1,
+    dchain(R1,C,Player).
+
+legal_move(R,C,Player) :-
+    position('.',R,C),
+
+    R1 is R - 1,
+    uchain(R1,C,Player).
+
+% an (r)chain consists of one
+% opponent disc and one adjacent
+% player disc or one opponent
+% disc and one adjacent (r)chain.
 
 rchain(R,C,Player) :-
     position(Opponent,R,C),
@@ -44,3 +73,57 @@ rchain(R,C,Player) :-
 
     C1 is C + 1,
     rchain(R,C1,Player).
+
+% left chain
+
+lchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    C1 is C - 1,
+    position(Player,R,C1).
+
+lchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    C1 is C - 1,
+    lchain(R,C1,Player).
+
+% downwards chain
+
+dchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    R1 is R + 1,
+    position(Player,R1,C).
+
+dchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    R1 is R + 1,
+    dchain(R1,C,Player).
+
+% upwards chain
+
+uchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    R1 is R - 1,
+    position(Player,R1,C).
+
+uchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    R1 is R - 1,
+    uchain(R1,C,Player).
