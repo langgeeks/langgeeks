7da043ec1787f7333e44a5a6e46c2db42d975c4b
Johan Lindberg <johan@pulp.se>
2010-06-08 11:12:29 +0200

    Added more test data (a 4x4 board) to identify all types of legal moves.
    Now that legal_move + rchain works, it should be quite simple to add rules
    for left, up and down as well. Running the program with the new test data
    results in one legal move found (R=1,C=0). Next I'll add rules for the other
    types of chains as well.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -1,9 +1,28 @@
 % reversi kata
 
+% test data
+% ---------------
 position('.',0,0).
-position('B',0,1).
-position('B',0,2).
-position('W',0,3).
+position('.',0,1).
+position('.',0,2).
+position('.',0,3).
+
+position('.',1,0).
+position('B',1,1).
+position('W',1,2).
+position('.',1,3).
+
+position('.',2,0).
+position('W',2,1).
+position('B',2,2).
+position('.',2,3).
+
+position('.',3,0).
+position('.',3,1).
+position('.',3,2).
+position('.',3,3).
+
+% ---------------
 
 legal_move(R,C,Player) :-
     position('.',R,C), % position must be empty
