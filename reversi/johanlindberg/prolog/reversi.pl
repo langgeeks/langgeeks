fca42655c8ec18172c08609ecc61749b55bc8160
Johan Lindberg <johan@pulp.se>
2010-06-08 11:00:42 +0200

    Added the recursive rule for rchain.
    The two rchain rules are pretty similar. I think I've got a pretty good
    understanding of how it works as well. Looking at traces are quite
    informative. Lesson learned: the order of definitions matters, it's the same
    order used when searching for a solution. If I would have switched places
    of the rchain rules it would potentially loop forever. In this situation it
    doesn't, because I change 'C' before calling rchain recursively. This has
    however bitten me quite a few times now.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -2,8 +2,8 @@
 
 position('.',0,0).
 position('B',0,1).
-%position('B',0,2).
-position('W',0,2).
+position('B',0,2).
+position('W',0,3).
 
 legal_move(R,C,Player) :-
     position('.',R,C), % position must be empty
@@ -17,3 +17,11 @@ rchain(R,C,Player) :-
 
     C1 is C + 1,
     position(Player,R,C1).
+
+rchain(R,C,Player) :-
+    position(Opponent,R,C),
+    Opponent \= '.',
+    Opponent \= Player,
+
+    C1 is C + 1,
+    rchain(R,C1,Player).
