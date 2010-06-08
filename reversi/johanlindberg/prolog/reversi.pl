2ea6469fa0624cb537cb7ff266459be6fd09a4ff
Johan Lindberg <johan@pulp.se>
2010-06-08 13:51:46 +0200

    Added test1.txt input file and a framework for reading files.
    I've struggled quite a bit with Prolog's I/O functionality. I found this
    snippet of code on http://en.wikibooks.org/wiki/Prolog/Input_and_Output and
    I'll use it as a foundation for getting the board representation from the
    text file. As is, process('filename') reads and prints the contents of a
    file.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -1,5 +1,16 @@
 % reversi kata
 
+process(File) :-
+        open(File, read, In),
+        get_char(In, Char1),
+        process_stream(Char1, In),
+        close(In).
+ 
+process_stream(end_of_file, _) :- !.
+process_stream(Char, In) :-
+        print(Char),
+        get_char(In, Char2),
+        process_stream(Char2, In).
 
 % a position is a legal move if it
 % is empty and has a chain to the
