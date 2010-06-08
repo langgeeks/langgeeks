775a7aa8c88ab860f651e74d8baa3bf2cf232efe
Johan Lindberg <johan@pulp.se>
2010-06-08 10:26:41 +0200

    Another test case for the match rule: '..W', which unfortunately works.

--- reversi/johanlindberg/prolog/reversi.pl
+++ reversi/johanlindberg/prolog/reversi.pl
@@ -1,13 +1,15 @@
 % reversi kata
 
 position('.',0,0).
-position('B',0,1).
+position('.',0,1).
 position('W',0,2).
 
 legal_move(R,C,Player) :-
-    position('.',R,C),
+    position('.',R,C), % position must be empty
+
     C1 is C + 1,
     position(Opponent,R,C1),
     Opponent \= Player,
+
     C2 is C + 2,
     position(Player,R,C2).
