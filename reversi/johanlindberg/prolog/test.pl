56741fe67d86d3d4933860a3ed55ea1356620e8c
Johan Lindberg <johan@pulp.se>
2010-06-09 20:42:05 +0200

    Changed one of the test cases to make it fail.
    Running this produced some results I didn't really expect. It fails with an
    uncaught exception in get_char/2. But I don't use get_char here!

--- reversi/johanlindberg/prolog/test.pl
+++ reversi/johanlindberg/prolog/test.pl
@@ -2,7 +2,8 @@
 
 all :-
     test('test1.txt', [5:3,4:6,3:5,6:4]),
-    test('test2.txt', [5:6,3:4]),
+%    test('test2.txt', [5:6,3:4]),
+    test('test2.txt', []),
     test('test3.txt', [6:4,6:5]).
 
 test(Filename, Expected) :-
