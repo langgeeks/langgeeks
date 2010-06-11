df09a815a2c3f09350c9fc2fa8910593ef112e5b
Johan Lindberg <johan@pulp.se>
2010-06-11 21:27:06 +0200

    Added find_rmoves/1 and modified to return position instead of match obj.

--- reversi/johanlindberg/erlang/reversi.erl
+++ reversi/johanlindberg/erlang/reversi.erl
@@ -1,7 +1,13 @@
 % reversi kata
 
 -module(reversi).
--export([find_moves/1]).
+-export([find_lmoves/1, find_rmoves/1]).
 
-find_moves(Board) ->
-    re:run(Board,"\.W+B",[]).
+find_lmoves(Board) ->
+    {_,[{Pos,_}]} = re:run(Board,"\.W+B",[]),
+    Pos.
+
+find_rmoves(Board) ->
+    {_,[{_,Pos}]} = re:run(Board,"BW+\.",[]),
+    Pos - 1.
+    
