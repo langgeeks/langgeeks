6794dfb90e84dfaa67520a62dbb6f8e75ef79dea
Johan Lindberg <johan@pulp.se>
2010-06-14 21:19:54 +0200

    Added more tests for find_lmoves and find_rmoves.

--- reversi/johanlindberg/erlang/reversi_tests.erl
+++ reversi/johanlindberg/erlang/reversi_tests.erl
@@ -12,16 +12,18 @@ opponent_test() ->
     "B" = reversi:opponent("A").
 
 find_lmoves_test() ->
-    0 = reversi:find_lmoves(".BW", "W"),
-    0 = reversi:find_lmoves(".BBW", "W"),
-    0 = reversi:find_lmoves(".BBBBBBW", "W"),
-    1 = reversi:find_lmoves("..BBBW", "W").    
+    [0] = reversi:find_lmoves(".BW", "W"),
+    [0] = reversi:find_lmoves(".BBW", "W"),
+    [0] = reversi:find_lmoves(".BBBBBBW", "W"),
+    [1] = reversi:find_lmoves("..BBBW", "W"),
+    
+    [0,3] = reversi:find_lmoves(".BW.BW", "W").
     
 find_rmoves_test() ->
-    2 = reversi:find_rmoves("WB.", "W"),
-    3 = reversi:find_rmoves("WBB.", "W"),
-    7 = reversi:find_rmoves("WBBBBBB.", "W"),
-    4 = reversi:find_rmoves("WBBB..", "W").    
+    [2] = reversi:find_rmoves("WB.", "W"),
+    [3] = reversi:find_rmoves("WBB.", "W"),
+    [7] = reversi:find_rmoves("WBBBBBB.", "W"),
+    [4] = reversi:find_rmoves("WBBB..", "W").    
 
 load_game_state_test() ->
     { board,
