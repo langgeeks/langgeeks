e79e09063ec220f4023ba47c31126a44f6117045
Johan Lindberg <johan@pulp.se>
2010-06-14 20:50:34 +0200

    Moved all tests into a separate module/file.
    eunit requires me to export all functions I want to test. I really don't like
    that. I'd prefer to keep my exports limited to the public API.

--- reversi/johanlindberg/erlang/reversi.erl
+++ reversi/johanlindberg/erlang/reversi.erl
@@ -10,25 +10,11 @@ opponent(Player) ->
 	true          -> "B"
     end.
 
-opponent_test() ->
-    "B" = reversi:opponent("W"),
-    "W" = reversi:opponent("B"),
-
-    "B" = reversi:opponent("."),
-    "B" = reversi:opponent("\n"),
-    "B" = reversi:opponent("A").
-
 find_lmoves(Board,Player) ->
     {_,[{Pos,_}]} = re:run(Board,
 			   string:join(["\\.",opponent(Player),"+",Player],""),
 			   []),
     Pos.
-
-find_lmoves_test() ->
-    0 = reversi:find_lmoves(".BW", "W"),
-    0 = reversi:find_lmoves(".BBW", "W"),
-    0 = reversi:find_lmoves(".BBBBBBW", "W"),
-    1 = reversi:find_lmoves("..BBBW", "W").    
 
 find_rmoves(Board,Player) ->
     {_,[{_,Pos}]} = re:run(Board,
@@ -36,12 +22,6 @@ find_rmoves(Board,Player) ->
 			   []),
     Pos - 1.
     
-find_rmoves_test() ->
-    2 = reversi:find_rmoves("WB.", "W"),
-    3 = reversi:find_rmoves("WBB.", "W"),
-    7 = reversi:find_rmoves("WBBBBBB.", "W"),
-    4 = reversi:find_rmoves("WBBB..", "W").    
-
 load_game_state(Filename) ->
     {ok,Input} = file:open(Filename,[read]),
     Lines      = contents_of_file(Input,[]),
@@ -72,12 +52,3 @@ make_cols([Row|Rows],Cols) ->
 	Rows -> make_cols(Rows,C)
     end.
 
-load_game_state_test() ->
-    { board,
-      {rows, Rows},
-      {cols, Cols},
-      {player, Player} } = reversi:load_game_state("test1.txt"),
-    "B" = Player,
-    8 = length(Rows),
-    8 = length(Cols).
-      
---
+++ reversi/johanlindberg/erlang/reversi_tests.erl
@@ -0,0 +1,34 @@
+% reversi kata
+
+-module(reversi_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+opponent_test() ->
+    "B" = reversi:opponent("W"),
+    "W" = reversi:opponent("B"),
+
+    "B" = reversi:opponent("."),
+    "B" = reversi:opponent("\n"),
+    "B" = reversi:opponent("A").
+
+find_lmoves_test() ->
+    0 = reversi:find_lmoves(".BW", "W"),
+    0 = reversi:find_lmoves(".BBW", "W"),
+    0 = reversi:find_lmoves(".BBBBBBW", "W"),
+    1 = reversi:find_lmoves("..BBBW", "W").    
+    
+find_rmoves_test() ->
+    2 = reversi:find_rmoves("WB.", "W"),
+    3 = reversi:find_rmoves("WBB.", "W"),
+    7 = reversi:find_rmoves("WBBBBBB.", "W"),
+    4 = reversi:find_rmoves("WBBB..", "W").    
+
+load_game_state_test() ->
+    { board,
+      {rows, Rows},
+      {cols, Cols},
+      {player, Player} } = reversi:load_game_state("test1.txt"),
+    "B" = Player,
+    8 = length(Rows),
+    8 = length(Cols).
+      
