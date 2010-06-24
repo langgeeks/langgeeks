117df60a582527bbb523a3e80264187e8c33e306
Johan Lindberg <johan@pulp.se>
2010-06-24 20:34:44 +0200

    Added test for diagonal moves.
    At the meeting last night we discussed various algorithms and solutions for
    this kata and slightly embarassed I had to admit that I hadn't implemented
    a search for diagonal moves. I intend to fix that now.

--- reversi/johanlindberg/prolog/declarative/test.pl
+++ reversi/johanlindberg/prolog/declarative/test.pl
@@ -3,7 +3,7 @@
 all :-
     asserteq(run, 'test1.txt', [5:3,4:6,3:5,6:4]),
     asserteq(run, 'test2.txt', [5:6,3:4]),
-    asserteq(run, 'test3.txt', [6:4,6:5]).
+    asserteq(run, 'test3.txt', [6:4,6:5,6:7]).
 
 asserteq(Functor, Args, Expected) :-
     test(Functor, Args, Expected, \=).
