4c3d76f6922892352f8cf8a8d7db1d44fc50359e
Johan Lindberg <johan@pulp.se>
2010-06-28 10:20:53 +0200

    Refactored load_game_state.
    It occured to me last night that I probably could use pattern matching more
    than I do. Since the format of the input file is not allowed to vary, I can
    pattern match against it directly. This gives us some more to type but will
    also be extra super clear about what's going on. Instead of using an
    algorithm to create rows, cols and diagonals I can merely state what
    variables should be included. I can even use the descriptive names for board
    positions (A5, E3 and such).

--- reversi/johanlindberg/erlang/regex/reversi.erl
+++ reversi/johanlindberg/erlang/regex/reversi.erl
@@ -75,8 +75,32 @@ load_game_state(Filename) ->
     {ok,Input} = file:open(Filename,[read]),
     Lines      = contents_of_file(Input,[]),
 
-    Rows       = lists:sublist(Lines,1,8),
-    Cols       = make_cols(Rows),
+    [[A1,A2,A3,A4,A5,A6,A7,A8],
+     [B1,B2,B3,B4,B5,B6,B7,B8],
+     [C1,C2,C3,C4,C5,C6,C7,C8],
+     [D1,D2,D3,D4,D5,D6,D7,D8],
+     [E1,E2,E3,E4,E5,E6,E7,E8],
+     [F1,F2,F3,F4,F5,F6,F7,F8],
+     [G1,G2,G3,G4,G5,G6,G7,G8],
+     [H1,H2,H3,H4,H5,H6,H7,H8]] = lists:sublist(Lines,1,8),
+
+    Rows       = [[A1,A2,A3,A4,A5,A6,A7,A8],
+		  [B1,B2,B3,B4,B5,B6,B7,B8],
+		  [C1,C2,C3,C4,C5,C6,C7,C8],
+		  [D1,D2,D3,D4,D5,D6,D7,D8],
+		  [E1,E2,E3,E4,E5,E6,E7,E8],
+		  [F1,F2,F3,F4,F5,F6,F7,F8],
+		  [G1,G2,G3,G4,G5,G6,G7,G8],
+		  [H1,H2,H3,H4,H5,H6,H7,H8]],
+
+    Cols       = [[A1,B1,C1,D1,E1,F1,G1,H1],
+		  [A2,B2,C2,D2,E2,F2,G2,H2],
+		  [A3,B3,C3,D3,E3,F3,G3,H3],
+		  [A4,B4,C4,D4,E4,F4,G4,H4],
+		  [A5,B5,C5,D5,E5,F5,G5,H5],
+		  [A6,B6,C6,D6,E6,F6,G6,H6],
+		  [A7,B7,C7,D7,E7,F7,G7,H7],
+		  [A8,B8,C8,D8,E8,F8,G8,H8]],
     Diagonals  = make_diagonals(Rows),
 
     Player     = lists:nth(9,Lines), 
@@ -89,16 +113,6 @@ contents_of_file(Input, Lines) ->
 	Line -> contents_of_file(Input,
 				 lists:append(Lines,
 					      [string:strip(Line,right,$\n)]))
-    end.
-
-make_cols(Rows) ->
-    make_cols(Rows, ["","","","","","","",""]).
-
-make_cols([Row|Rows],Cols) ->
-    F = fun(R,C) -> lists:append(C,[R])  end,
-    C = lists:zipwith(F, Row, Cols),
-    if Rows == [] -> C;
-       true       -> make_cols(Rows,C)
     end.
 
 make_diagonals(Rows) ->
