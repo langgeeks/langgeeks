163d52355069f99a2ea3408ca13b83b2a03ea7f4
Gert Meulyzer <cel357@gmail.com>
2010-07-18 17:14:28 +0200

    added some data definitions for players and locations

--- reversi/gertmeulyzer/haskell/Reversi.hs
+++ reversi/gertmeulyzer/haskell/Reversi.hs
@@ -1,6 +1,15 @@
 import System.Environment
 
+data Player = B | W
+data Location = Black | White | Empty
+
+
 main = do
   args <- getArgs
   boardString <- readFile (head args)
-  putStrLn boardString
+  checkLines $ lines boardString
+  
+checkLines :: [String] -> IO [()]
+checkLines = mapM putStrLn
+
+-- validMoveOnLine :: String -> String -> Bool
