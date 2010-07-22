f810ccc5118ef5ef1c847030619bef1409779d28
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2010-07-22 14:48:02 +0200

    Updated run.sh and compile.se for scala 2.8.0 final.

--- reversi/jorgen99/scala/iteration1/compile.sh
+++ reversi/jorgen99/scala/iteration1/compile.sh
@@ -1,1 +1,1 @@
-fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar *.scala
+fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar *.scala
--- reversi/jorgen99/scala/iteration1/run.sh
+++ reversi/jorgen99/scala/iteration1/run.sh
@@ -1,2 +1,2 @@
-fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar *.scala
-scala -cp ./:lib/junit-4.8.1.jar:lib/scalatest-1.2-for-scala-2.8.0.RC7-SNAPSHOT.jar org.junit.runner.JUnitCore ReversiSuite
+fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar *.scala
+scala -cp ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar org.junit.runner.JUnitCore ReversiSuite
