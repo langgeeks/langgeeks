baa3a1997229177a0288fc2e1c6189cfbf2881c7
Johan Lindberg <johan@pulp.se>
2010-07-27 18:13:04 +0200

    First stab at making the test pass.
    Evaluating test_findMove still returns False but there are a few things that
    have happened. First of all, I've modified the function signature to return a
    list of Ints instead and secondly I've added a function called findMoveInRow
    which takes care of the searching. At the moment it returns the position of
    the first player char it encounters.
    
    I suspect I'll do another function to search for and check that positions
    found actually are valid moves. Plenty of things undecided and that I need to
    learn.
    
    Still not comfy enough with QuickCheck to put it to use. Make quirky work-
    around will have to do a while longer.

--- reversi/johanlindberg/haskell/matrix/reversi.hs
+++ reversi/johanlindberg/haskell/matrix/reversi.hs
@@ -1,7 +1,13 @@
 -- Reversi kata for langgeeks
 
-findMove :: Char -> [String] -> Int 
-findMove player board = 0
+findMove :: Char -> [String] -> [Int]
+findMove player board = [findMoveInRow player row 0 | row <- board] 
+
+findMoveInRow :: Char -> String -> Int -> Int
+findMoveInRow player [] pos = -1
+findMoveInRow player (x:xs) pos = if x == player
+                                  then pos
+                                  else findMoveInRow player xs pos+1
 
 test_findMove :: Bool
-test_findMove = findMove 'B' ["...BW..."] == 5
+test_findMove = findMove 'B' ["...BW..."] == [5]
