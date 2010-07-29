b5492b714844645e7cab19f05362eea3416b03f1
Johan Lindberg <johan@pulp.se>
2010-07-29 07:15:01 +0200

    Added some more test cases for findChain.
    Ok, so findChain seems to work as expected. IT should be fairly easy to
    modify findMoveInRow to make that test pass as well. At the moment it finds
    the wrong char (player) but that's easily fixed.

--- reversi/johanlindberg/haskell/matrix/reversi.hs
+++ reversi/johanlindberg/haskell/matrix/reversi.hs
@@ -17,8 +17,11 @@ findChain player (x:xs) pos = if x /= player
                               else pos /= 0
 
 test_findChain :: Bool
-test_findChain = findChain 'B' "WWB.." 0 == True &&
-                 findChain 'B' "B.." 0 == False
+test_findChain = findChain 'B' "WWB.." 0  == True  &&
+                 findChain 'B' "B.." 0    == False &&
+                 findChain 'B' ".WWB.." 0 == False &&
+                 findChain 'B' "..." 0    == False &&
+                 findChain 'B' "W.." 0    == False
 
 test_findMoveInRow :: Bool
 test_findMoveInRow = findMoveInRow 'B' "...BW..." 0 == 5
