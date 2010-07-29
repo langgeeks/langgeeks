b6812adb075cbda3fb58e42a71afc1b4da73778d
Johan Lindberg <johan@pulp.se>
2010-07-29 07:55:26 +0200

    Modified the tests for findMove and findMoveInRow since they don't match.
    Since the implementation of findChain only searches to the right the current
    state of findMove and findMoveInRow don't really match the tests. Also, I'm
    hoping that I can do some high-level mojo thing in findMove to make the
    low-level implementation of findMoveInRow work for all cases anyway.

--- reversi/johanlindberg/haskell/matrix/reversi.hs
+++ reversi/johanlindberg/haskell/matrix/reversi.hs
@@ -5,7 +5,7 @@ findMove player board = [findMoveInRow player row 0 | row <- board]
 
 findMoveInRow :: Char -> String -> Int -> Int
 findMoveInRow player [] pos = -1
-findMoveInRow player (x:xs) pos = if x == '.' && findChain player xs 0
+findMoveInRow player (x:xs) pos = if x == '.' && findChain player xs 0 
                                   then pos
                                   else findMoveInRow player xs (pos+1)
 
@@ -25,7 +25,7 @@ test_findChain = findChain 'B' "WWB.." 0  == True  &&
                  findChain 'B' "W.." 0    == False
 
 test_findMoveInRow :: Bool
-test_findMoveInRow = findMoveInRow 'B' "...BW..." 0 == 5
+test_findMoveInRow = findMoveInRow 'W' "...BW..." 0 == 2
 
 test_findMove :: Bool
-test_findMove = findMove 'B' ["...BW..."] == [5]
+test_findMove = findMove 'W' ["...BW..."] == [2]
