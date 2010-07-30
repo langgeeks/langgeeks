7de7c6ee93e8c667882bdd7d92719abbeaa21968
Johan Lindberg <johan@pulp.se>
2010-07-30 15:28:52 +0200

    Modified the findChain function to take an increment parameter.
    In order to make this work both ways (searching left and right) but without
    having to define (basically) the same function two times I want this function
    to take an increment parameter for starters and a function that traverses the
    list as another parameter. This rewrite changes nothing in terms of making
    tests pass. Everything that's been done is modifiying function calls to match
    the new implementation.
    
    Next up is making findChain take a function as a parameter that will be used
    for traversing the list. Tail is hard coded into the function definition at
    the moment.

--- reversi/johanlindberg/haskell/matrix/reversi.hs
+++ reversi/johanlindberg/haskell/matrix/reversi.hs
@@ -5,24 +5,24 @@ findMove player board = [findMoveInRow player row 0 | row <- board]
 
 findMoveInRow :: Char -> String -> Int -> Int
 findMoveInRow player [] pos = -1
-findMoveInRow player (x:xs) pos = if x == '.' && findChain player xs 0 
+findMoveInRow player (x:xs) pos = if x == '.' && findChain player xs 0 1
                                   then pos
                                   else findMoveInRow player xs (pos+1)
 
-findChain :: Char -> String -> Int -> Bool
-findChain player [] pos = False
-findChain player (x:xs) pos = if x /= player
-                              then if x /= '.'
-                                   then findChain player xs (pos+1)
-                                   else False
-                              else pos /= 0
+findChain :: Char -> String -> Int -> Int -> Bool
+findChain player [] pos inc  = False
+findChain player row pos inc = if (head row) /= player
+                               then if (head row) /= '.'
+                                    then findChain player (tail row) (pos + inc) inc
+                                    else False
+                               else pos /= 0
 
 test_findChain :: Bool
-test_findChain = findChain 'B' "WWB.." 0  == True  &&
-                 findChain 'B' "B.." 0    == False &&
-                 findChain 'B' ".WWB.." 0 == False &&
-                 findChain 'B' "..." 0    == False &&
-                 findChain 'B' "W.." 0    == False
+test_findChain = findChain 'B' "WWB.." 0 1  == True  &&
+                 findChain 'B' "B.." 0 1    == False &&
+                 findChain 'B' ".WWB.." 0 1 == False &&
+                 findChain 'B' "..." 0 1    == False &&
+                 findChain 'B' "W.." 0 1    == False
 
 test_findMoveInRow :: Bool
 test_findMoveInRow = findMoveInRow 'W' "...BW..." 0 == 2
