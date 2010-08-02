e1a35388c286ee8c7164560aacd0d64df4437b9d
Johan Lindberg <johan@pulp.se>
2010-08-02 13:03:05 +0200

    Refactoring functions from if/then/else to guarded expressions.
    Trying out some different ways of writing these functions. Hoping they'll end
    up more readable and understandable. Nested if/then/else is a bit cryptic to
    read so guarded expressions or pattern matching is better since they clearly
    enumerate all cases the code is intended to handle. I wish all programming
    languages had this feature. I imagine it'll be difficult to go back once I
    get used to it.

--- reversi/johanlindberg/haskell/matrix/reversi.hs
+++ reversi/johanlindberg/haskell/matrix/reversi.hs
@@ -8,19 +8,17 @@ reverseIndex :: [Int] -> [Int]
 reverseIndex [] = []
 reverseIndex moves = map (\x -> 7 - x) moves
 
-findMoveInRow :: Char -> String -> Int -> [Int]
-findMoveInRow player [] pos = []
-findMoveInRow player (x:xs) pos = if x == '.' && findChain player xs 0
-                                  then [pos] ++ findMoveInRow player xs (pos+1)
-                                  else findMoveInRow player xs (pos+1)
+findMovesInRow :: Char -> String -> Int -> [Int]
+findMovesInRow player [] pos = []
+findMovesInRow player (x:xs) pos | x == '.'
+                                   && findChain player xs 0 = [pos] ++ findMovesInRow player xs (pos+1)
+                                 | otherwise                = findMovesInRow player xs (pos+1)
 
 findChain :: Char -> String -> Int -> Bool
 findChain player [] pos     = False
-findChain player (x:xs) pos = if x /= player
-                              then if x /= '.'
-                                   then findChain player xs (pos + 1)
-                                   else False
-                              else pos /= 0
+findChain player (x:xs) pos | x == player = pos /= 0
+                            | x /= '.'    = findChain player xs (pos+1)
+                            | otherwise   = False
 
 test_findChain :: Bool
 test_findChain = findChain 'B' "WWB.." 0  == True  &&
@@ -29,9 +27,10 @@ test_findChain = findChain 'B' "WWB.." 0  == True  &&
                  findChain 'B' "..." 0    == False &&
                  findChain 'B' "W.." 0    == False
 
-test_findMoveInRow :: Bool
-test_findMoveInRow = findMoveInRow 'W' "...BW..." 0 == [2] &&
-                     findMoveInRow 'W' ".BW..BW." 0 == [0,4]
+test_findMovesInRow :: Bool
+test_findMovesInRow = findMovesInRow 'W' "...BW..." 0 == [2] &&
+                      findMovesInRow 'W' ".BW..BW." 0 == [0,4] &&
+                      findMovesInRow 'W' "...WB..." 0 == []
 
 --test_findMove :: Bool
 --test_findMove = findMove 'W' ["...BW...","...WB..."] == [2,5]
