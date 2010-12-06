839d77d485ef4969f479744a1d627d8602b42749
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2010-12-06 10:50:27 +0100

    Removed compile.sh and run.sh since I'm using sbt now.

--- reversi/jorgen99/scala/iteration1/compile.sh
+++
@@ -1,1 +0,0 @@
-fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar *.scala
--- reversi/jorgen99/scala/iteration1/run.sh
+++
@@ -1,2 +0,0 @@
-fsc -classpath ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar *.scala
-scala -cp ./:lib/junit-4.8.1.jar:lib/scalatest-1.2.jar org.junit.runner.JUnitCore ReversiSuite
